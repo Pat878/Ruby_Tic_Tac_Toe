924c68879b98a17d3d967708af0358dd30864009
Patrick Mellon <mellon.patrick.j@gmail.com>
2017-03-23 22:21:59 -0400

    Update playermove functions with if statements

--- TicTacToe.rb
+++ TicTacToe.rb
@@ -24,8 +24,24 @@ end
 def player1Move
   puts "Player 1, make your move."
   move = gets.chomp.to_i
-  if move == 1
+  if move == 1 && @square1 != "O"
 @square1 = "X"
+elsif move == 2 && @square2 != "O"
+@square2 = "X"
+elsif move == 3 && @square3 != "O"
+@square3 = "X"
+elsif move == 4 && @square4 != "O"
+@square4 = "X"
+elsif move == 5 && @square5 != "O"
+@square5 = "X"
+elsif move == 6 && @square6 != "O"
+@square6 = "X"
+elsif move == 7 && @square7 != "O"
+@square7 = "X"
+elsif move == 8 && @square8 != "O"
+@square8 = "X"
+elsif move == 9 && @square9 != "O"
+@square9 = "X"
   end
 newBoard
 end
@@ -42,11 +58,26 @@ end
 def player2Move
   puts "Player 2 make your move."
   move = gets.chomp.to_i
-  if move == 2
+  if move == 1 && @square1 != "X"
+    @square1 = "O"
+  elsif move == 2 && @square2 != "X"
   @square2 = "O"
-
+elsif move == 3 && @square3 != "X"
+@square3 = "O"
+elsif move == 4 && @square4 != "X"
+@square4 = "O"
+elsif move == 5 && @square5 != "X"
+@square5 = "O"
+elsif move == 6 && @square6 != "X"
+@square6 = "O"
+elsif move == 7 && @square7 != "X"
+@square7 = "O"
+elsif move == 8 && @square8 != "X"
+@square8 = "O"
+elsif move == 9 && @square9 != "X"
+@square9 = "O"
   end
-  newBoard
+newBoard
   end
 
 end
